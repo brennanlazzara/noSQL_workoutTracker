5d2967ed92b4737bff5e104916062bd574898bd8
Your Brennan Predmore <brennnann@gmail.com>
2020-04-07 14:35:33 -0500

    server.js init // express require and PORT 8082

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+let PORT = process.env.PORT || 8081;
+
+let app = express();
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
